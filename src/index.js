cc0e625b2d852bded59f94d85c76031799f74502
Alexandra <65849986+sanechka25@users.noreply.github.com>
2021-03-12 10:10:03 +0300

    docs: upload file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+  if(n<0){
+    let res = Math.abs(n)
+    res = String(res).split('').reverse().join('')
+    return res
+  }
+  n = String(n).split('').reverse().join('');
+  return n;
 }
